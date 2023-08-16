9fc39c93821a2f3cc0131def551957ca2583473d
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-08-16 16:37:07 +0200

    created logout button

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,9 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
+
 
 orders = Blueprint("orders", __name__, url_prefix="")
 
 
 @orders.route("/")
+@login_required
 def index():
-    return "Order Up!"
-
+    return render_template('orders.html')
--- app/routes/session.py
+++ app/routes/session.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from ..forms import LoginForm
 from ..models import Employee
 
@@ -20,3 +20,8 @@ def login():
         login_user(employee)
         return redirect(url_for("orders.index"))
     return render_template("login.html", form=form)
+
+@session.route('/logout', methods=["POST"])
+def logout():
+    logout_user()
+    return redirect(url_for('.login'))
